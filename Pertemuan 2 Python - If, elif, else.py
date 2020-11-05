05bdbee02ff7e26b16c0b2a5169ff5806c66e2b5
Nicholas Nanda <nikolasnanda@gmail.com>
2020-11-05 22:19:23 +0700

    Update Pertemuan 2 Python - If, elif, else.py
    menambahkan komen/ keterangan

--- Pertemuan 2 Python - If, elif, else.py
+++ Pertemuan 2 Python - If, elif, else.py
@@ -1,3 +1,6 @@
+# a = 4
+# b = 4
+
 # if a < b:
 #     print("a kurang dari b")
 # elif a > b:
@@ -10,10 +13,8 @@
 #shorthand if else
 #print("A") if a > b else print("B")
 
-# a = 4
-# b = 4
 
-# #biasa
+# # if else biasa
 # if a != b: 
 #     if a > b: 
 #         print("a lebih besar dari b") 
@@ -29,7 +30,7 @@
 
 # angka = 27
 # bilangan = 27
-
+# #if else biasa
 # if angka % 2 == 0 or angka != bilangan:
 #     if angka > 0:
 #         print(str(angka) + " Adalah Genap Positif")
@@ -41,26 +42,30 @@
 #     else:
 #         print(str(angka)+" Adalah Ganjil Negatif")
     
-
+# #tenary
 # print(str(bilangan) + " Adalah Genap Positif" if bilangan > 0 and bilangan % 2 == 0 
 # else str(bilangan) + " Adalah Genap Negatif" if bilangan % 2 == 0 
 #      else str(bilangan) + " Adalah Ganjil Positif" if bilangan > 0 and bilangan % 2 == 1 
 #           else str(bilangan) + " Adalah Ganjil Negatif")
 
+#assign variable linear
 x = b = c = 2
-y = 5
 
+y = 5
+#statement biasa
 if 0 < x and x < y and y < 10: # 0 < x < y < 10
     print(str(x + y) + "<- hasil penjumlahan")
     print(str(b + c) + " <- hasil penjumlahan")
 else:
     print("gagal menjumlah")
 
+#statement linear
 if not (0 < x < y < 10):
     print(str(x * y) + "<- hasil perkalian")
 else:
     print("gagal mengkalikan")
 
+#penggunaan negasi dan jika didalam (in) suatu sting/list/tuple/dict/dan sebagainya
 kata = ["apel","jeruk","mangga"]
 if "lemon" not in kata:
     print("kami tidak jual lemon")
@@ -68,10 +73,9 @@ else:
     print("ga ketemu kak")
 
 
-# # Print tanpa enter
-
-# print("Hello") 
+# # Print dengan enter
+# print("Hello")
 # print("World") 
 
-# print("Hello", end =",") #isi dalam end adalah akhiran dari yang di print *default \n atau new line
+# print("Hello", end =",") #isi dalam end adalah akhiran dari yang di print *default \n atau new line, biar hilangin enter isikan dengan ="" atau =" "
 # print("World") 
